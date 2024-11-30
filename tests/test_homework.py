f2dcf47c1c6d1cc03263674632d4a724d3d576a2
Julianh1206 <jsaavedrae@unal.edu.co>
2024-11-29 19:54:58 -0500

    PrimerIntento

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -5,6 +5,6 @@ import os
 
 def test_homework():
     """Test homework."""
-    assert os.path.exists("files/output/file1.txt")
-    assert os.path.exists("files/output/file2.txt")
-    assert os.path.exists("files/output/file3.txt")
+    assert os.path.exists("files/input/file1.txt")
+    assert os.path.exists("files/input/file2.txt")
+    assert os.path.exists("files/input/file3.txt")
